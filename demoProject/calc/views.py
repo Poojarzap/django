54598a39898b38b2edc621cdf458c2e808dda956
Poojarzap <pooja.r@zapcg.com>
2024-02-21 17:52:46 +0530

    Calculated sum of 2 numbers and displayed result in result.html

--- demoProject/calc/views.py
+++ demoProject/calc/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"home.html",{'Name':'Pooja','Age':'22'})
+
+def add(request):
+    n1=int(request.GET['num1'])
+    n2=int(request.GET['num2'])
+    result=n1+n2
+    return render(request,'result.html',{'Result':result})
