7af71e48eb35f3a5bc4af401c73e909e629e76eb
Poojarzap <pooja.r@zapcg.com>
2024-02-22 17:50:12 +0530

    Passing dynamic data in HTML using classes and objects

--- demoProject/travello/views.py
+++ demoProject/travello/views.py
@@ -6,4 +6,17 @@ from .models import Destination
 def index(request):
     dest1=Destination()
     dest1.name = 'Udupi'
-    return render(request,"index.html",{'dest1':dest1})
+    dest1.desc = 'City with beautiful hearts'
+    dest1.price = 1000
+
+    dest2=Destination()
+    dest2.name = 'Mulki'
+    dest2.desc = 'City with beautiful cats'
+    dest2.price = 999
+
+    dest3=Destination()
+    dest3.name = 'Brahmavara'
+    dest3.desc = 'City with beautiful dogs'
+    dest3.price = 1900
+
+    return render(request,"index.html",{'dest1':dest1,'dest2':dest2,'dest3':dest3})
