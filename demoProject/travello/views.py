425da17bc783a6bb70eb4140cbf23822c8162c08
Poojarzap <pooja.r@zapcg.com>
2024-02-22 17:12:43 +0530

    Used template and created a Destination class in models.py

--- demoProject/travello/views.py
+++ demoProject/travello/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Destination
+
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    dest1=Destination()
+    dest1.name = 'Udupi'
+    return render(request,"index.html",{'dest1':dest1})
