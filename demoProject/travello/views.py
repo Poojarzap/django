80f466f6144ff74f67d44a1b766fbfec33516106
Poojarzap <pooja.r@zapcg.com>
2024-02-23 10:34:21 +0530

    Added for loop to iterate the destinations stored in list of objects, and made changes to image path at top of index.html

--- demoProject/travello/views.py
+++ demoProject/travello/views.py
@@ -7,16 +7,20 @@ def index(request):
     dest1=Destination()
     dest1.name = 'Udupi'
     dest1.desc = 'City with beautiful hearts'
+    dest1.img = 'destination_1.jpg'
     dest1.price = 1000
 
     dest2=Destination()
     dest2.name = 'Mulki'
     dest2.desc = 'City with beautiful cats'
+    dest2.img = 'destination_2.jpg'
     dest2.price = 999
 
     dest3=Destination()
     dest3.name = 'Brahmavara'
     dest3.desc = 'City with beautiful dogs'
+    dest3.img = 'destination_3.jpg'
     dest3.price = 1900
 
-    return render(request,"index.html",{'dest1':dest1,'dest2':dest2,'dest3':dest3})
+    dest = [dest1,dest2,dest3]
+    return render(request,"index.html",{'dests':dest})
