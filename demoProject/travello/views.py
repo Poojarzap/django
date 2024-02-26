0690ac500be9dc7f4b5ef1fa627f561fda8e6e67
Poojarzap <pooja.r@zapcg.com>
2024-02-26 16:59:03 +0530

    Created superuser and added destinations to database, Also fetched data from database and displayed on screen

--- demoProject/travello/views.py
+++ demoProject/travello/views.py
@@ -4,26 +4,5 @@ from .models import Destination
 
 # Create your views here.
 def index(request):
-    dest1=Destination()
-    dest1.name = 'Udupi'
-    dest1.desc = 'City with beautiful hearts'
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 1000
-    dest1.offer=True
-
-    dest2=Destination()
-    dest2.name = 'Mulki'
-    dest2.desc = 'City with beautiful cats'
-    dest2.img = 'destination_2.jpg'
-    dest2.price = 999
-    dest2.offer=True
-
-    dest3=Destination()
-    dest3.name = 'Brahmavara'
-    dest3.desc = 'City with beautiful dogs'
-    dest3.img = 'destination_3.jpg'
-    dest3.price = 1900
-    dest3.offer=False
-
-    dests = [dest1,dest2,dest3]
+    dests=Destination.objects.all()
     return render(request,"index.html",{'dests':dests})
