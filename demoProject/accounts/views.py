25e066bb635d88763bc4d54036fd9c2bc517e8e2
Poojarzap <pooja.r@zapcg.com>
2024-02-27 11:14:35 +0530

    Created a login page, and passing error messaged directly on the webpage

--- demoProject/accounts/views.py
+++ demoProject/accounts/views.py
@@ -1,6 +1,28 @@
+import django
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import User,auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+
+from django.contrib import messages
+
 # Create your views here.
+
+
+def login(request):
+    if request.method=='POST':
+        username=request.POST['uname']
+        upass=request.POST['pass']
+        user = authenticate(request,username=username,password=upass)
+        if user is not None:
+            django.contrib.auth.login(request, user)
+            print("LOGIN successfull")
+            return redirect("/")
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+    
 def register(request):
     if request.method=='POST':
         fname=request.POST['first_name']
@@ -14,18 +36,33 @@ def register(request):
         if pass1==pass2:
             if User.objects.filter(username=uname).exists():
                 print('USERNAME taken')
+                messages.info(request,'Username taken')
+                return redirect('register')
             elif User.objects.filter(email=mail).exists():
                 print('EMAIL taken')
+                messages.info(request,'Email taken')
+                return redirect('register')
             else:
             
             #Creating user from build-in function 
                 user=User.objects.create_user(username=uname,password=pass1,email=mail,first_name=fname,last_name=lname)
                 user.save()
                 print('USER CREATED')
+                return redirect('login')
+
         else:
             print('PASSWORD DOES NOT MATCH')
+            messages.info(request,'Password doesnot match')
+            return redirect('register')
         return redirect('/') #Return to home page after registering...
     else:
     #When register tab is pressed in index.html
         return render(request,"register.html")
+    
+
+
+
+
+        
+    
     
